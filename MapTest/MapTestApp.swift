b7da998cec533d6d9835802e2c6af2bcb316e3aa
Dan Dolinar <dandolinar@me.com>
2022-04-09 22:51:35 -0700

    Initial Commit

---
+++ MapTest/MapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestApp.swift
+//  MapTest
+//
+//  Created by Dan on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
